c55cdf09d21a1694970abee4a9b4b8deafba2a0d
jaq1997 <aguiarjaqueline1997@gmail.com>
2025-04-16 12:26:31 -0300

    pronto para vercel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    open: true
+    port: 3000,
   },
-  build: {
-    outDir: 'dist',
-    minify: true,
-    sourcemap: false
-  }
-})
+});
